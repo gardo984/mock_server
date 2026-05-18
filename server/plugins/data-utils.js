c8ef0866c42194bd38a80d3b081a141d6ba431ca
Manuel Lazo <gardo984@gmail.com>
2026-05-18 09:53:58 -0500

    adding node generators

--- server/plugins/data-utils.js
+++ server/plugins/data-utils.js
@@ -1,6 +1,15 @@
 'use strict'
 
+const { promisify } = require('utils')
 const fp = require('fastify-plugin')
+const timeout = promisify(setTimeout)
+
+
+const orders = {
+	A1: { total: 3 },
+	A2: { total: 7 },
+	B1: { total: 101 },
+}
 const catToPrefix = {
 	electronics: 'A',
 	confectionery: 'B',
@@ -12,12 +21,36 @@ const calculateID = (idPrefix, data) => {
 	return `${idPrefix}${next}`
 }
 
+async function* realtimeOrdersSimulator() {
+	const ids = Object.keys(orders)
+	while (true) {
+		const delta = Math.floor(Math.random() * 7) + 1
+		const id = ids[Math.floor(Math.random() * ids.length)]
+		orders[id].total += delta
+		const { total } = orders[id]
+		yield JSON.stringify({ id, total })
+		await timeout(1500)
+		// or yield new Promise(resolve => setTimeout(resolve, 1500))
+	}
+}
+
+function* currentOrders(category) {
+	const idPrefix = catToPrefix[category]
+	if (!idPrefix) return
+	const ids = Object.keys(orders).filter((id) => id[0] === idPrefix)
+	for (const id of ids) {
+		yield JSON.stringify({ id, ...orders[id] })
+	}
+}
+
 module.exports = fp(async function (fastify, opts) {
+	fastify.decorate('currentOrders', currentOrders)
+	fastify.decorate('realtimeOrders', realtimeOrdersSimulator)
 	fastify.decorateRequest('mockDataInsert', function insert(category, data) {
 		const request = this
 		const idPrefix = catToPrefix[category]
 		const id = calculateID(idPrefix, data)
-		const payload = {id, ...request.body}
+		const payload = { id, ...request.body }
 		data.push(payload)
 	})
 })
